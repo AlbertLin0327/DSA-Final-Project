6acc99c70b7582756d4f8f8984d0b70e038ae4c4
AlbertLin0327 <linhsinkai@gmail.com>
2020-06-02 20:03:42 +0800

    Add main.cpp and test_data

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void add(string &route){
+	stringstream mail;
+	ifstream input(route, ios::binary);
+	
+	// read file into a string-stream
+	mail << input.rdbuf();
+	input.close();
+
+	// processing the data
+	return;
+}
+
+void remove(int &id){
+
+	// remove message-id = id from all data structures
+	return;
+}
+
+void longest(){
+
+	// return the longest message
+}
+
+void query(stringstream &conditions){
+
+	// parse the condition and find the target mail
+	return;
+}
+
+int main(){
+
+	// fast I/O and also avoid std::cout << std::endl
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	return 0;
+}
