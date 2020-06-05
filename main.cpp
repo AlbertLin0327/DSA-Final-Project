054eae99870452a03a118617f9806838053209a5
hermes926 <hermes926@gmail.com>
2020-06-06 01:28:31 +0800

    test test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
 using namespace std;
+
+class date{
+	public:
+		int getdate()
+			return date_value;
+		date(int y, int m, int d, int t){
+			year = y;
+			month = m;
+			day = d;
+			time = t;
+			date_value = y * 10000000 + m * 1000000 + d * 10000 + t; 
+		}
+
+	private:
+		int year, month, day, time;
+		long long int date_value;
+}
+
+class mail{
+	public:
+		
+	private:
+		int id;
+		string from, to;
+		date senttime;
+		
+};
 
 void add(string &route){
 	stringstream mail;
@@ -8,7 +41,7 @@ void add(string &route){
 	// read file into a string-stream
 	mail << input.rdbuf();
 	input.close();
-
+	cout << mail.str();
 	// processing the data
 	return;
 }
@@ -35,6 +68,21 @@ int main(){
 	// fast I/O and also avoid std::cout << std::endl
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	unordered_map<string, int> months;
+	months["January"] = 1;
+    months["February"] = 2;
+    months["March"] = 3;
+    months["April"] = 4;
+    months["May"] = 5;
+    months["June"] = 6;
+    months["July"] = 7;
+    months["August"] = 8;
+    months["September"] = 9;
+    months["October"] = 10;
+    months["November"] = 11;
+    months["December"] = 12;
 
+	string test = "test_data\\mail1";
+	add(test);
 	return 0;
 }
